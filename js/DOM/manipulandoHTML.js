d336f5249c6abcb82ba505cc92e0067652bc720a
Erik Samuel Pérez Saavedra <eperez@BETTERWARE>
2022-01-30 13:12:19 -0600

    maniplandohtml2

--- js/DOM/manipulandoHTML.js
+++ js/DOM/manipulandoHTML.js
@@ -2,6 +2,9 @@ const inputName = document.querySelector (".nombre")
 const inputPassword = document.querySelector(".password")
 const forma = document.querySelector("#formulario")
 
+
+
+
 inputPassword.addEventListener('input',()=>{
     inputPassword.type="text" 
     setTimeout(()=>{
@@ -13,16 +16,20 @@ const alerta = document.createElement('DIV')
 alerta.classList.add('alerta') 
 
 const nombre = document.querySelector (".nombre").value
-const password = inputPassword.value 
+const password = document.querySelector (".password").value
 
-if (nombre==='' && password==''){
-   // alert(nombre)
-    alerta.textContent =' Todos los campos son requeridos' 
-    alerta.classList.add('error')
-}
-else{
-    alerta.textContent ='...Todo bien'
-    alerta.classList.add('exito')
-}
+forma.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    if (nombre==='' && password==''){
+       // alert(nombre)
+        alerta.textContent =' Todos los campos son requeridos' 
+        alerta.classList.add('error')
+    }
+    else{
+        alerta.textContent ='...Todo bien'
+        alerta.classList.add('exito')
+    }
+    forma.appendChild(alerta)
+})
 
-forma.appendChild(alerta)
+
